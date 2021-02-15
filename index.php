61b791d1df30e66630be4eab416e29344e669429
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-15 11:54:39 +0200

    add two routes and run application

--- index.php
+++ index.php
@@ -2,24 +2,17 @@
 require_once "vendor/autoload.php";
 
 use app\core\Application;
-use app\core\Router;
+
 
 
 $app = new Application();
 
+$app->router->get('/', function () {
+    return "this is home page";
+});
 
-$router = new Router();
+$app->router->get('/about', function (){
+    return "this is about page";
+});
 
-
-
-//$router = new Router();
-//
-//$router->get('/', function (){
-//    return "this is home page";
-//});
-//
-//$router->get('/about', function (){
-//    return "this isabout page";
-//});
-//
-//$app->run();
+$app->run();
