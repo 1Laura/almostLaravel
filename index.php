0918b3608cf66850f75a2df2a9e6f85c0218059b
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-15 09:31:35 +0200

    index.php road map

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "Almost Lara";
+$app = new Aplication();
+
+$router = new Router();
+
+$router->get('/', function (){
+    return "this is home page";
+});
+
+$router->get('/about', function (){
+    return "this isabout page";
+});
+
+$app->run();
