09712dc4d123f3d1a79e42ffad33d9c4d75b1182
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-15 09:56:25 +0200

    added core Application.php and Router.php classes

---
+++ core/Application.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace app\core;
+
+
+/**
+ * Class Application
+ *
+ * This is main application
+ *
+ * @package app\core
+ */
+class Application
+{
+    public function __construct()
+    {
+        echo "This is Application constructor";
+    }
+
+}
