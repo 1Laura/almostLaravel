cb60c93389af8f22f269f4a831b3d3be75692526
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-15 11:52:27 +0200

    Public property Router.php and router init in constructor + fn run

--- core/Application.php
+++ core/Application.php
@@ -13,9 +13,26 @@ namespace app\core;
  */
 class Application
 {
+
+    /**
+     * This is instance of Router class
+     *
+     * We will need routing in all our application. So we will have it as a property;
+     *
+     * @var Router
+     */
+    public Router $router;
+
     public function __construct()
     {
-        echo "This is Application constructor";
+//        echo "This is Application constructor";
+        $this->router = new Router();
+    }
+
+
+    public function run()
+    {
+        $this->router->resolve();
     }
 
 }
