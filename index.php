31bf517e0cda29e51efa7df3fc9655c2eaa1c758
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-17 09:46:42 +0200

    added posts page

--- index.php
+++ index.php
@@ -3,9 +3,8 @@ require_once "vendor/autoload.php";
 
 use app\core\Application;
 
-
-
 $app = new Application();
+
 
 $app->router->get('/', function () {
     return "this is home page";
@@ -14,5 +13,10 @@ $app->router->get('/', function () {
 $app->router->get('/about', function (){
     return "this is about page";
 });
+
+$app->router->get('/posts', function (){
+    return "this is posts page";
+});
+
 
 $app->run();
