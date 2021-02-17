6b679e50199f2f23520586cd37c2ac485f7a1b7c
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-17 09:50:49 +0200

    added method to a resolve fn

--- core/Router.php
+++ core/Router.php
@@ -31,17 +31,23 @@ class Router
      * @var array
      */
     protected array $routes = [];
+    /**
+     * @var Request
+     */
     public Request $request;
 
 
     public function __construct($request)
     {
         $this->request = $request;
-        echo "This is Router constructor" . PHP_EOL;
-
+//        echo "This is Router constructor" . PHP_EOL;
     }
 
 
+    /**
+     * @param $path
+     * @param $callback
+     */
     public function get($path, $callback)
     {
         $this->routes['get'][$path] = $callback;
@@ -50,10 +56,25 @@ class Router
 
     public function resolve()
     {
-//        var_dump($this->routes);
-        var_dump($this->request->getPath());
-        exit;
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
 
+        var_dump($this->routes);
+        var_dump($path);
+        var_dump($method);
+
+        //trying to run a route from routes array
+        $callback = $this->routes[$method][$path] ?? false;
+        //if there is no such route added, we say not exist
+
+        if ($callback === false):
+            echo "Page doesnt exists";
+            die();
+        endif;
+
+        // page does exist we call user function
+
+        echo call_user_func($callback);
     }
 
 }
