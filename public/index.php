48509c6504d1818fc8a4f3ddbd6891387b7bdaf0
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-17 10:44:22 +0200

    autoload.php

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+require_once '../vendor/autoload.php';
+
+use app\core\Application;
+
+$app = new Application();
+
+
+$app->router->get('/', function () {
+    return "this is home page";
+});
+
+//
+$app->router->get('/about', function () {
+    return "this is about page";
+});
+
+$app->run();
