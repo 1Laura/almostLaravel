fcbe30828ed3b6a13b0ae76c239ba0bfac5ece88
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-22 10:47:48 +0200

    testing and seeing . env values

--- public/index.php
+++ public/index.php
@@ -9,6 +9,20 @@ use app\core\AuthController;
 //var_dump(dirname(__DIR__));
 //exit();
 
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+$config = [
+    'db' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD']
+    ]
+];
+
+var_dump($config);
+exit();
+
 
 $app = new Application(dirname(__DIR__));
 
