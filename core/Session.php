48839581f7dbbb51e15304ba7b5567b276802a94
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-23 10:44:31 +0200

    isUserLoggedIn()

--- core/Session.php
+++ core/Session.php
@@ -11,4 +11,18 @@ class Session
 //        session_destroy();
         session_start();
     }
+
+    /**
+     * checks is user is logged in
+     *
+     * @return bool
+     */
+    public static function isUserLoggedIn(): bool
+    {
+        if (isset($_SESSION['userId'])) {
+            return true;
+        }
+        return false;
+    }
+
 }
