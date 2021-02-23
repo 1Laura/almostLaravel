8bbabd9f83ff23c06791d4012bfa6e86f8637cd4
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-23 11:21:50 +0200

    fix sql query

--- model/PostModel.php
+++ model/PostModel.php
@@ -21,7 +21,7 @@ class PostModel
     //return Object Array
     public function getPosts()
     {
-        $sql = "SELECT posts.title, posts.body, users.name, users.email, posts.id AS postId, users.id AS userId,
+        $sql = "SELECT posts.title, posts.body, users.name, users.email, posts.postId AS postId, users.id AS userId,
  posts.created AS postCreated, users.created AS userCreated FROM posts INNER JOIN users ON posts.userId = users.id
  ORDER BY posts.created DESC";
 
